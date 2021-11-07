56acad5a7a83c719bbe0803ab7178e12c4269a20
GisnetzEU <gismontero.mail@gmail.com>
2021-11-07 19:06:21 +0100

    sin enviar email

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 use Symfony\Component\Mailer\MailerInterface;
+use App\Form\ContactoFormType;
 
 class DefaultController extends AbstractController
 {
