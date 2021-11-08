b1ba9853e4814a982ea0abb00d829afa537ed9dc
gisnetzEU <gismontero.mail@gmail.com>
2021-11-08 09:07:00 +0100

    envia bien el email

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use App\Form\ContactoFormType;
 
 class DefaultController extends AbstractController
 {
